1c163c5fb2a938d9753f0b614ff001ade30af372
Stefanie Guenther <guenther@mathcces.rwth-aachen.de>
2018-07-03 14:22:21 +0200

    Update createhistory to handle the .plt format.

--- TestCases/cylinder_afc/createhistory.py
+++ TestCases/cylinder_afc/createhistory.py
@@ -7,22 +7,23 @@ import sys
 
 # parse command line arguments
 args = sys.argv
-if (len(args) == 3):
-  filename   = str(args[1])  # Get the filenames without the last "_000*.dat"
-  npt        = int(args[2])  # Get the number of temporal processors
+if (len(args) == 4):
+  filename   = str(args[1])  # Get the filenames without the last "_000*.*"
+  fileformat = str(args[2])  # Get the fileformat ".plt" or ".dat" 
+  npt        = int(args[3])  # Get the number of temporal processors
 else:
   print("\nERROR while parsing argument list !")
-  print("Usage: python3 createhistory.py <FileNameWithoutLast'_000*.dat'> <NumberOfTemporalProcessors\n")
+  print("Usage: python3 createhistory.py <FileNameWithoutLast'_000*.*'> <.plt or .dat> <NumberOfTemporalProcessors\n")
   stop
 
 # Open the output filename
-outfile = open(filename + '.dat', 'w')
+outfile = open(filename + fileformat, 'w')
 
 # Iterate over all local history files
 for inpt in range(npt):
     
     # Open the local history file and read it
-    currfile = open(filename + '_%05i'%(inpt) + '.dat','r') 
+    currfile = open(filename + '_%05i'%(inpt) + fileformat,'r') 
     currfilelines = currfile.readlines()
     currfilesize  = len(currfilelines)
 
